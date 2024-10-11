27a4766583d137b684e914f567e03902ee4912b8
Rico Saupe <145867813+RicoSaupe@users.noreply.github.com>
2024-10-11 20:48:55 +0200

    added first structure to template

---
+++ Build.fs
@@ -0,0 +1,153 @@
+open System
+
+open Fake.Core
+open Fake.DotNet
+open Fake.IO
+open Fake.Tools
+
+let execContext = Context.FakeExecutionContext.Create false "build.fsx" []
+Context.setExecutionContext (Context.RuntimeContext.Fake execContext)
+
+let skipTests = Environment.hasEnvironVar "yolo"
+let release = ReleaseNotes.load "RELEASE_NOTES.md"
+
+let templatePath = "./Content/.template.config/template.json"
+let defaultTemplatePath = "./Content/default"
+let templateProj = "FIT.BackgroundJob.proj"
+let templateName = "FIT Background Job"
+let version = Environment.environVarOrDefault "VERSION" ""
+let nupkgDir = Path.getFullName "./nupkg"
+
+let nupkgPath =
+    System.IO.Path.Combine(nupkgDir, $"FIT.BackgroundJob.%s{version}.nupkg")
+
+let result =
+    DotNet.exec (fun x -> { x with DotNetCliPath = "dotnet" }) "tool" "restore"
+
+if not result.OK then
+    failwithf "`dotnet %s %s` failed" "tool" "restore"
+
+let formattedRN =
+    release.Notes
+    |> List.map (sprintf "* %s")
+    |> String.concat "\n"
+
+Target.create "Clean" (fun _ -> Shell.cleanDirs [ nupkgDir ])
+
+let msBuildParams msBuildParameter : MSBuild.CliArguments =
+    { msBuildParameter with DisableInternalBinLog = true }
+
+Target.create "Pack" (fun _ ->
+    Shell.regexReplaceInFileWithEncoding
+        "  \"name\": .+,"
+        ("  \"name\": \""
+         + templateName
+         + " v"
+         + version
+         + "\",")
+        Text.Encoding.UTF8
+        templatePath
+
+    let releaseNotesUrl = Environment.environVarOrDefault "RELEASE_NOTES_URL" ""
+
+    DotNet.pack
+        (fun args ->
+            { args with
+                Configuration = DotNet.BuildConfiguration.Release
+                OutputPath = Some nupkgDir
+                MSBuildParams = msBuildParams args.MSBuildParams
+                Common =
+                    { args.Common with
+                        CustomParams =
+                            Some(sprintf "/p:PackageVersion=%s /p:PackageReleaseNotes=\"%s\"" version releaseNotesUrl) } })
+        templateProj)
+
+Target.create "Install" (fun _ ->
+    let unInstallArgs = $"uninstall FIT.BackgroundJob"
+
+    DotNet.exec (fun x -> { x with DotNetCliPath = "dotnet" }) "new" unInstallArgs
+    |> ignore // Allow this to fail as the template might not be installed
+
+    let installArgs = $"install \"%s{nupkgPath}\""
+
+    DotNet.exec (fun x -> { x with DotNetCliPath = "dotnet" }) "new" installArgs
+    |> fun result ->
+        if not result.OK then
+            failwith $"`dotnet new %s{installArgs}` failed with %O{result}")
+
+let psi exe arg dir (x: ProcStartInfo) : ProcStartInfo =
+    { x with
+        FileName = exe
+        Arguments = arg
+        WorkingDirectory = dir }
+
+let run exe arg dir =
+    let result = Process.execWithResult (psi exe arg dir) TimeSpan.MaxValue
+
+    if not result.OK then
+        (failwithf "`%s %s` failed: %A" exe arg result.Errors)
+
+Target.create "TemplateExecutionTests" (fun _ ->
+    let cmd = "run"
+    let args = "--project tests/Tests.fsproj"
+    let result = DotNet.exec (fun x -> { x with DotNetCliPath = "dotnet" }) cmd args
+
+    if not result.OK then
+        failwithf "`dotnet %s %s` failed" cmd args)
+
+Target.create "DefaultTemplateTests" (fun _ ->
+    let cmd = "run"
+    let args = "RunTestsHeadless --project Build.fsproj"
+
+    let result =
+        DotNet.exec
+            (fun x ->
+                { x with
+                    DotNetCliPath = "dotnet"
+                    WorkingDirectory = defaultTemplatePath })
+            cmd
+            args
+
+    if not result.OK then
+        failwithf "`dotnet %s %s` failed" cmd args)
+
+Target.create "Release" (fun _ ->
+    let nugetApiKey = Environment.environVarOrFail "NUGET_API_KEY"
+
+    let nugetArgs =
+        $"""push "*.nupkg" --api-key {nugetApiKey} --source https://api.nuget.org/v3/index.json"""
+
+    let result =
+        DotNet.exec
+            (fun x ->
+                { x with
+                    DotNetCliPath = "dotnet"
+                    WorkingDirectory = nupkgDir })
+            "nuget"
+            nugetArgs
+
+    if not result.OK then
+        failwithf "`dotnet %s` failed" "nuget push")
+
+open Fake.Core.TargetOperators
+
+"Clean"
+=?> ("TemplateExecutionTests", not skipTests)
+=?> ("DefaultTemplateTests", not skipTests)
+==> "Pack"
+==> "Install"
+==> "Release"
+|> ignore
+
+[<EntryPoint>]
+let main args =
+    try
+        match args with
+        | [| target |] -> Target.runOrDefault target
+        | _ -> Target.runOrDefault "Install"
+
+        0
+    with
+    | e ->
+        printfn "%A" e
+        1
--- README.md
+++ README.md
@@ -1,2 +1,73 @@
-# FIT.BackgroundJob.Template
-A template for a F# Backgroundjob eg. Windows Service
+# FIT F# Background Job
+
+An opinionated F# Windows Background job Template
+
+### Configuration
+
+* **global.json** pinned to .NET 8
+* **VS Code** settings to hide inlay hints by default
+* **.gitignore** with common F# settings
+* **src/FIT.Background** folder structure
+
+### Pre-installed dotnet tools
+
+* **Fantomas v6** pre-configured with `.editorconfig`
+* **Paket v8** with the following dependencies:
+  * FSharp.Core
+  * FSToolkit.ErrorHandling
+
+## To execute the template
+
+```bash
+dotnet new fit-backgroundjob -o MyBackgroundjob
+```
+
+will give you a folder structure as follows:
+
+```
+.config
+    dotnet-tools.json
+.paket
+    Paket.Restore.targets
+.vscode
+    settings.json
+src
+    FIT.BackgroundJob
+        FIT.BackgroundJob.fsproj
+        paket.references
+
+        Readme.md
+FIT.BackgroundJob.sln
+.editorconfig
+.gitignore
+global.json
+paket.dependencies
+paket.lock
+```
+
+```bash
+dotnet tool restore
+cd src/FIT.BackgroundJob
+dotnet run
+# or use visual studio, visual studio code or Rider
+```
+
+### Adding a package
+
+```bash
+cd src/FIT.BackgroundJob
+dotnet paket add <package name>
+```
+
+### Removing a package
+
+```bash
+cd src/FIT.BackgroundJob
+dotnet paket remove <package name>
+```
+
+### Safely updating all dependencies
+
+```bash
+dotnet paket update --keep-major
+```
